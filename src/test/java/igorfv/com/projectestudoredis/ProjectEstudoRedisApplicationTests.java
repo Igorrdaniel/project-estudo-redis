126990bc137826252a4102c3c0a2a670998d5547
Igor Daniel <igordaniel11.id@gmail.com>
2024-08-20 16:43:29 -0300

    Initial commit

---
+++ src/test/java/igorfv/com/projectestudoredis/ProjectEstudoRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package igorfv.com.projectestudoredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEstudoRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
